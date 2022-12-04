fa22e18cb764c4e544029ccec5a09cc088ea63d4
Axel <axel.hammarberg@gmail.com>
2022-12-04 12:18:04 +0100

    Cleaned up day 4 code

--- day_4/content/main.hs
+++ day_4/content/main.hs
@@ -7,25 +7,22 @@ main :: IO ()
 main = do 
         inpStr <- readFile "../input/input4.txt"
         let contents = lines inpStr
-        print (sum (map overlapp (map split contents)))
-        print (sum (map overlapp2 (map split contents)))
+        print . length . filter overlapp_fully $ map split contents
+        print . length . filter overlapp_any $ map split contents
 
 
 split :: String -> (Int, Int, Int, Int)
 split s = (low_1, high_1, low_2, high_2) where
     dash_indices = elemIndices '-' s
-    comma_index = fromJust (elemIndex ',' s)
-    low_1 = read (take (dash_indices !! 0) s) :: Int
-    high_1 = read (drop (1 + dash_indices !! 0) (take comma_index s)) :: Int
-    low_2 = read (drop (1 + comma_index) (take (dash_indices !! 1) s)) :: Int
-    high_2 = read (drop (1 + dash_indices !! 1) s) :: Int
+    comma_index = fromJust $ elemIndex ',' s
+    low_1 = read $ take (dash_indices !! 0) s
+    high_1 = read $ drop (1 + dash_indices !! 0) $ take comma_index s
+    low_2 = read $ drop (1 + comma_index) $ take (dash_indices !! 1) s
+    high_2 = read $ drop (1 + dash_indices !! 1) s
 
-overlapp :: (Int, Int, Int, Int) -> Int
-overlapp (low_1, high_1, low_2, high_2) = (res) where
-    res = if (low_1 <= low_2 && high_1 >= high_2) || (low_2 <= low_1 && high_2 >= high_1) then 1
-    else 0
+overlapp_fully :: (Int, Int, Int, Int) -> Bool
+overlapp_fully (low_1, high_1, low_2, high_2) =
+    (low_1 <= low_2 && high_1 >= high_2) || (low_2 <= low_1 && high_2 >= high_1)
 
-overlapp2 :: (Int, Int, Int, Int) -> Int
-overlapp2 (low_1, high_1, low_2, high_2) = (res) where
-    res = if (low_1 <= low_2 && high_1 >= low_2) || (low_1 <= high_2 && high_1 >= high_2) || (overlapp (low_1, high_1, low_2, high_2) == 1) then 1
-    else 0
+overlapp_any :: (Int, Int, Int, Int) -> Bool
+overlapp_any (low_1, high_1, low_2, high_2) = not (high_1 < low_2 || low_1 > high_2)
